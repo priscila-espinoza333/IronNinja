a2f69738d83cac49e7b9bb56d44d1850b42ac73f
Pris <pr.espinoza@duocuc.cl>
2023-10-24 15:52:29 -0300

    first commit

---
+++ Drink.cs
@@ -0,0 +1,11 @@
+class Drink : IConsumable
+{
+    public string Name {get;set;}
+    public int Calories {get;set;}
+    public bool IsSpicy {get;set;}
+    public bool IsSweet {get;set;}
+    
+    // Implement a GetInfo Method
+    // Add a constructor method
+}   
+
